ff80b49688647ff330b83efef8801758305042e0
nachocuru406 <icurutchet@udesa.edu.ar>
2025-11-17 15:29:10 -0300

    Arranque con la api

---
+++ js/index.js
@@ -0,0 +1,23 @@
+fetch('https://dummyjson.com/products')
+.then(function(response) {
+  return response.json()
+})
+.then(function(data) {
+  console.log(data);
+  let aleatorias = document.querySelector(".Seccion_1")
+  let producto = ""
+    for (let i = 0; i < 10; i++) {
+        console.log(data.products[i]);
+        let perfiles = data.products[i]
+        producto += `<article class="Article_1">
+                    <img class="Imagenes_1" src="${data.products[i].images[0]}" alt="Imagen">
+                    <h3 class="Sub-titulo_1">${data.products[i].title}</h3>
+                    <p class="Texto_1">${data.products[i].description}</p>
+                    <a class="Boton_1" href="./category.html">VER MAS</a>
+                </article>`
+    }
+aleatorias.innerHTML= producto
+})
+.catch(function(error) {
+  console.log("Error: " + error);
+})
