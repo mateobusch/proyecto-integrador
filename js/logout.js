941bdfdfbddeac632a843f93941c0991f9e78bdc
Mateo Busch <mbusch@udesa.edu.ar>
2025-11-20 15:17:10 -0300

    Separe en dos archivos js distintos el saludo y logout

---
+++ js/logout.js
@@ -0,0 +1,20 @@
+if (recuperoStorage.length >= 1){
+    let logoutLink = document.querySelector("#logoutLink")
+    logoutLink.addEventListener('click', function (e){
+        e.preventDefault();
+        alert("Cerraste sesion")
+        localStorage.removeItem("userName");
+        let usuarioHeader = document.querySelector(".Usuario_header")
+
+        let headerOriginal = `<li class="Log_in_header">
+                            <a class="Usuario_1_header" href="./login.html">LOGIN</a>
+                            </li>
+                            <li class="Registro_header">
+                            <a class="Usuario_1_header" href="./registro.html">REGISTRO</a>
+                            </li>`;
+    usuarioHeader.innerHTML = headerOriginal
+    usuarioHeader.style.margin = "auto"
+    usuarioHeader.style.width = "198.35px"
+    usuarioHeader.style.height = "48.5px"
+    });
+}
--- js/saludo.js
+++ js/saludo.js
@@ -14,20 +14,4 @@ if (recuperoStorage.length >= 1){
     header_menu.style.width = "790px"
     usuarioHeader.style.margin = "20px 10px 0px 0px"
     usuarioHeader.style.width = "245px"
-    let logoutLink = document.querySelector("#logoutLink")
-    logoutLink.addEventListener('click', function (e){
-        e.preventDefault();
-        alert("Cerraste sesion")
-        localStorage.removeItem("userName");
-        let headerOriginal = `<li class="Log_in_header">
-                            <a class="Usuario_1_header" href="./login.html">LOGIN</a>
-                            </li>
-                            <li class="Registro_header">
-                            <a class="Usuario_1_header" href="./registro.html">REGISTRO</a>
-                            </li>`;
-    usuarioHeader.innerHTML = headerOriginal
-    usuarioHeader.style.margin = "auto"
-    usuarioHeader.style.width = "198.35px"
-    usuarioHeader.style.height = "48.5px"
-    });
 }
