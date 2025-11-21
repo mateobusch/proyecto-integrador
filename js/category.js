c2554e0caf1c89d2dbc55fe6559834f964d23e63
nachocuru406 <icurutchet@udesa.edu.ar>
2025-11-21 19:21:05 -0300

    Termine la pagina de categoria y los filtros de las categorias de remeras y cosmeticos

--- js/category.js
+++ js/category.js
@@ -1,23 +1,46 @@
 let queryString = location.search;
-let queryParams = new URLSearchParams(queryString);
-fetch('https://dummyjson.com/products')
+let queryObj = new URLSearchParams(queryString);
+let categoriaId = queryObj.get("id");
+
+
+if (categoriaId == null) {
+    categoriaId = "mens-shirts";
+}
+
+let contenedor = document.querySelector(".Seccion_1_category");
+
+
+contenedor.innerHTML = "";
+
+let url = "https://dummyjson.com/products/category/" + categoriaId;
+
+fetch(url)
 .then(function(response) {
-  return response.json()
+    return response.json();
 })
 .then(function(data) {
-  console.log(data);
-  let category = document.querySelector(".Seccion_1_category")
-  let producto_3 = ""
-    for (let i = 0; i < 6; i++) {
-        console.log(data.products[i]);
-        let perfiles = data.products[i]
-        producto_3 += `<article class="Article_1">
-                    <img class="Imagenes_1" src="${data.products[i].images[0]}" alt="Imagen">
-                    <h3 class="Sub-titulo_1">${data.products[i].title}</h3>
-                    <p class="Texto_1">${data.products[i].description}</p>
-                    <a class="Boton_1" href="./product.html?id=${data.products[i].id}">VER MAS</a>
-                </article>`
+
+    let productos = data.products;
+    let html = "";
+
+    for (let i = 0; i < productos.length; i++) {
+
+        let producto = productos[i];
+
+        html +=
+        '<article class="Article_1">' +
+            '<img class="Imagenes_1" src="' + producto.thumbnail + '" alt="Imagen">' +
+            '<h3 class="Sub-titulo_1">' + producto.title + '</h3>' +
+            '<p class="Texto_1">' + producto.description + '</p>' +
+            '<h4 class="Precio">€' + producto.price + '</h4>' +
+            '<a class="Boton_1" href="./product.html?id=' + producto.id + '">COMPRAR</a>' +
+        '</article>';
     }
-category.innerHTML= producto_3
-category.style.height="auto"
+
+    contenedor.innerHTML = html;
+    contenedor.style.height="auto"
 })
+.catch(function(error){
+    console.log("Error cargando productos", error);
+    contenedor.innerHTML = "<p>Error al cargar la categoría.</p>";
+});
